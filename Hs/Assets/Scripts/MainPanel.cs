c99813d0a232a2fa3576cd4692e319f566241414
BarakpaevOlzhas <olzhas.barakpaev@mail.ru>
2019-02-19 23:54:05 +0600

    Add project

---
+++ Hs/Assets/Scripts/MainPanel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour
+{
+    [SerializeField]
+    private Transform root;
+
+    [SerializeField]
+    private SettingMenu settingMenu;
+
+    [SerializeField]
+    private Button continueButton, settingButton, exitButton;   
+
+    void Start()
+    {
+        exitButton.onClick.AddListener(ButtonClickExit);
+        continueButton.onClick.AddListener(ButtonClickContinue);
+        settingButton.onClick.AddListener(ButtonClickSetting);
+    }
+
+    private void ButtonClickContinue()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    private void ButtonClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void Init()
+    {
+        root.gameObject.SetActive(true);
+    }
+
+    private void ButtonClickSetting()
+    {
+        root.gameObject.SetActive(false);
+        settingMenu.Init();
+    }
+
+    private void OnDestroy()
+    {
+        exitButton.onClick.RemoveAllListeners();
+        continueButton.onClick.RemoveAllListeners();
+        settingButton.onClick.RemoveAllListeners();
+    }
+}
---
+++ Hs/Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Root;
+
+    [SerializeField]
+    private MainPanel mainPanel;
+
+    [SerializeField]
+    private Button buttonBack;
+
+    void Start()
+    {
+        buttonBack.onClick.AddListener(ButtonClickBack);
+    }    
+
+    private void ButtonClickBack()
+    {
+        Root.gameObject.SetActive(false);
+        mainPanel.Init();
+    }
+
+    public void Init()
+    {
+        Root.gameObject.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        buttonBack.onClick.RemoveAllListeners();
+    }
+}
